ba107c9760b000c915007ade73f0054267823287
Leone <leone011@163.com>
2023-09-22 02:31:00 +0800

    check

--- src/main/java/com/yanyu/logmaster/service/impl/LogstashMasterServiceImpl.java
+++ src/main/java/com/yanyu/logmaster/service/impl/LogstashMasterServiceImpl.java
@@ -7,12 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 
 
